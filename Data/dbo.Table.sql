3802d631d71c5555c133126583e1a19e1bc1381a
2222958@my.ipleiria.pt <2222958@my.ipleiria.pt>
2024-11-26 16:02:38 +0000

    Database

---
+++ Data/dbo.Table.sql
@@ -0,0 +1,41 @@
+CREATE TABLE [dbo].[Container]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NOT NULL, 
+    [creation_datetime] TIMESTAMP NOT NULL DEFAULT GETDATE(), 
+    [parent] INT NOT NULL
+)
+
+
+CREATE TABLE [dbo].[Notification]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NOT NULL,
+	[creation_datetime] DATETIME2 NOT NULL DEFAULT GETDATE(), 
+    [parent] INT NOT NULL, 
+    [event] INT NOT NULL, 
+    [endpoint] VARCHAR(50) NOT NULL, 
+    [enabled] BIT NOT NULL DEFAULT 0,
+
+)
+
+
+CREATE TABLE [dbo].[Record]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NOT NULL, 
+    [content] VARCHAR(50) NOT NULL,
+	[creation_datetime] DATETIME2 NOT NULL DEFAULT GETDATE(), 
+    [parent] INT NOT NULL,
+
+)
+
+
+CREATE TABLE [dbo].[Application] (
+    [Id]                INT           NOT NULL,
+    [name]              VARCHAR (50)  NOT NULL,
+    [creation_datetime] DATETIME2 (7) DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
