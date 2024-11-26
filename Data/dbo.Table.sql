4633be693fb59e04380c212f4924340d27dff8d0
2222958@my.ipleiria.pt <2222958@my.ipleiria.pt>
2024-11-26 16:27:51 +0000

    db script done 3.0

--- Data/dbo.Table.sql
+++ Data/dbo.Table.sql
@@ -1,31 +1,32 @@
 CREATE TABLE [dbo].[Container]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [name] VARCHAR(50) NOT NULL, 
+    [name] VARCHAR(50) NOT NULL UNIQUE, 
     [creation_datetime] DATETIME2 NOT NULL DEFAULT GETDATE(), 
     [parent] INT NOT NULL
 )
 
 
-CREATE TABLE [dbo].[Notification]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [name] VARCHAR(50) NOT NULL,
-	[creation_datetime] DATETIME2 NOT NULL DEFAULT GETDATE(), 
-    [parent] INT NOT NULL, 
-    [event] INT NOT NULL, 
-    [endpoint] VARCHAR(255) NOT NULL, 
-    [enabled] BIT NOT NULL DEFAULT 0, 
-    CONSTRAINT [FK_Notification_ToContainer] FOREIGN KEY ([parent]) REFERENCES [Container]([id]),
+CREATE TABLE [dbo].[Notification] (
+    [Id]                INT           NOT NULL,
+    [name]              VARCHAR (50)  NOT NULL UNIQUE,
+    [creation_datetime] DATETIME2 (7) DEFAULT (getdate()) NOT NULL,
+    [parent]            INT           NOT NULL,
+    [event]             INT           NOT NULL,
+    [endpoint]          VARCHAR (255)  NOT NULL,
+    [enabled]           BIT           DEFAULT ((0)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Notification_ToContainer] FOREIGN KEY ([parent]) REFERENCES [dbo].[Container] ([Id])
+);
 
-)
+
 
 
 
 CREATE TABLE [dbo].[Record]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [name] VARCHAR(50) NOT NULL, 
+    [name] VARCHAR(50) NOT NULL UNIQUE, 
     [content] VARCHAR(50) NOT NULL,
 	[creation_datetime] DATETIME2 NOT NULL DEFAULT GETDATE(), 
     [parent] INT NOT NULL, 
@@ -36,7 +37,7 @@ CREATE TABLE [dbo].[Record]
 
 CREATE TABLE [dbo].[Container] (
     [Id]                INT           NOT NULL,
-    [name]              VARCHAR (50)  NOT NULL,
+    [name]              VARCHAR (50)  NOT NULL UNIQUE,
     [creation_datetime] DATETIME2 (7) DEFAULT (getdate()) NOT NULL,
     [parent]            INT           NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC), 
