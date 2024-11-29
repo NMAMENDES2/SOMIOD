46cd75825f04a05218298d4039a5b70c06fcc93c
NMAMENDES2 <nmamendes163@gmail.com>
2024-11-29 18:30:07 +0000

    comecei o app manager

---
+++ Project/SOMIOD/ApplicationManagement/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ApplicationManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getApplications = new System.Windows.Forms.Button();
+            this.richTextBoxListApplications = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // getApplications
+            // 
+            this.getApplications.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.getApplications.Location = new System.Drawing.Point(29, 38);
+            this.getApplications.Name = "getApplications";
+            this.getApplications.Size = new System.Drawing.Size(123, 23);
+            this.getApplications.TabIndex = 0;
+            this.getApplications.Text = "List applications";
+            this.getApplications.UseVisualStyleBackColor = false;
+            this.getApplications.Click += new System.EventHandler(this.getApplications_Click);
+            // 
+            // richTextBoxListApplications
+            // 
+            this.richTextBoxListApplications.Location = new System.Drawing.Point(29, 67);
+            this.richTextBoxListApplications.Name = "richTextBoxListApplications";
+            this.richTextBoxListApplications.Size = new System.Drawing.Size(199, 195);
+            this.richTextBoxListApplications.TabIndex = 3;
+            this.richTextBoxListApplications.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxListApplications);
+            this.Controls.Add(this.getApplications);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getApplications;
+        private System.Windows.Forms.RichTextBox richTextBoxListApplications;
+    }
+}
+
---
+++ Project/SOMIOD/ApplicationManagement/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using RestSharp;
+using App = SOMIOD.Models.Application;
+using System.Xml.Serialization;
+using System.IO; // Havia conflitos com outra keyword de .net ent tem de ser assim
+
+
+
+namespace ApplicationManagement
+{
+    public partial class Form1 : Form
+    {
+
+        string url = @"http://localhost:57806/api/somiod/";
+        RestClient cliente = null;
+        public Form1()
+        {
+            InitializeComponent();
+            cliente = new RestClient(url);
+        }
+
+        private void getApplications_Click(object sender, EventArgs e)
+        {
+
+            richTextBoxListApplications.Clear();
+            try
+            {
+                var request = new RestRequest("/", Method.Get);
+                request.AddHeader("somiod-locate", "application");
+                request.AddHeader("Content-Type", "application/xml");
+                request.RequestFormat = DataFormat.Xml;
+
+                var response = cliente.Execute(request);
+                /*
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    richTextBoxListApplications.AppendText($"Error: {response.StatusCode} - {response.StatusDescription}\n");
+                    return;
+                }
+                */
+                var serializer = new XmlSerializer(typeof(App));
+                using (var reader = new StringReader(response.Content))
+                {
+                    var data = (App)serializer.Deserialize(reader);
+                    richTextBoxListApplications.AppendText(data.name + "\n");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
