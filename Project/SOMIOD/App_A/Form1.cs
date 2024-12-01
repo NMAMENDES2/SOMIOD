176fb21ce92f4b986eaf581b15556c64050b9e0b
AndreMRosa1 <161585909+AndreMRosa1@users.noreply.github.com>
2024-12-01 15:51:19 +0000

    Added App_A and App_B initial prototype

---
+++ Project/SOMIOD/App_A/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_A
+{
+    public partial class App_A : Form
+    {
+        private bool isOn = false;
+
+        public App_A()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.off;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isOn)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = Properties.Resources.off;
+            }
+            else
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = Properties.Resources.on; 
+            }
+
+            isOn = !isOn;
+        }
+    }
+}
