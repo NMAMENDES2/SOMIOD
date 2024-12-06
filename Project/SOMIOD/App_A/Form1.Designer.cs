e820a397a65087a13d66e00b13f35d632d95ea9a
AndreMRosa1 <161585909+AndreMRosa1@users.noreply.github.com>
2024-12-06 01:25:40 +0000

    Changes made to app A and B

--- Project/SOMIOD/App_A/Form1.Designer.cs
+++ Project/SOMIOD/App_A/Form1.Designer.cs
@@ -28,20 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(115, 319);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -61,7 +50,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(313, 354);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "App_A";
             this.Text = "App A";
@@ -74,7 +62,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
--- Project/SOMIOD/App_A/Form1.cs
+++ Project/SOMIOD/App_A/Form1.cs
@@ -4,17 +4,27 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using RestSharp;
 using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace App_A
 {
     public partial class App_A : Form
     {
-        private bool isOn = false;
         MqttClient mqttClient;
+        string[] topics = { "light_bulb" };
+        byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE };
+
+        string baseURI = @"http://localhost:57806/api/somiod";
+        RestClient client = null;
 
         public App_A()
         {
@@ -24,22 +34,111 @@ namespace App_A
         private void Form1_Load(object sender, EventArgs e)
         {
             pictureBox1.Image = Properties.Resources.off;
+
+            createOpertion();
+
+            try
+            {
+                mqttClient = new MqttClient("127.0.0.1");
+                mqttClient.Connect(Guid.NewGuid().ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to connect to broker: {ex.Message}");
+                return;
+            }
+
+            if (!mqttClient.IsConnected)
+            {
+                MessageBox.Show("MQTT client is not connected");
+                return;
+            }
+
+            mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+            mqttClient.Subscribe(topics, qosLevels);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void createOpertion()
         {
-            if (isOn)
+            client = new RestClient(baseURI);
+
+            Application application = new Application()
             {
-                pictureBox1.Image.Dispose();
-                pictureBox1.Image = Properties.Resources.off;
-            }
-            else
+                name = "Lighting",
+            };
+
+            Container container = new Container()
             {
-                pictureBox1.Image.Dispose();
-                pictureBox1.Image = Properties.Resources.on; 
+                name = "light_bulb",
+                parent = 10,
+            };
+
+            var appRequest = new RestRequest("/create", Method.Post);
+            appRequest.RequestFormat = DataFormat.Xml;
+            appRequest.AddXmlBody(application);
+            var responseApp = client.Execute(appRequest);
+
+            var contRequest = new RestRequest("/create", Method.Post);
+            contRequest.RequestFormat = DataFormat.Xml;
+            contRequest.AddXmlBody(container);
+            var responseCont = client.Execute(contRequest);
+
+            if (responseApp.StatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show($"Failed to create application: {responseApp.StatusDescription}");
             }
 
-            isOn = !isOn;
+            if (responseCont.StatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show($"Failed to create container: {responseCont.StatusDescription}");
+            }
+        }
+
+        private void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string topic = e.Topic;
+            string msg = Encoding.UTF8.GetString(e.Message);
+
+            if (topic != topics[0])
+                MessageBox.Show("WRONG TOPIC");
+
+            this.Invoke((MethodInvoker)(() =>
+            {
+                if (msg.Equals("on", StringComparison.OrdinalIgnoreCase))
+                {
+                    pictureBox1.Image = Properties.Resources.on;
+                }
+                else if (msg.Equals("off", StringComparison.OrdinalIgnoreCase))
+                {
+                    pictureBox1.Image = Properties.Resources.off;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid message");
+                }
+            }));
+        }
+
+        private void App_A_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (mqttClient != null && mqttClient.IsConnected)
+            {
+                mqttClient.Disconnect();
+            }
+        }
+
+        public class Application
+        {
+            [XmlElement("name")]
+            public string name { get; set; }
+        }
+
+        public class Container
+        {
+            [XmlElement("name")]
+            public string name { get; set; }
+            [XmlElement("parent")]
+            public int parent { get; set; }
         }
     }
 }
